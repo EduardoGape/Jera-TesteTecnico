27273d1b29945f7dc7845f04e0e18d429163a922
Eduardo Leite <102864156+EduardoGape@users.noreply.github.com>
2024-05-26 22:00:11 -0300

    Delete api directory

--- api/src/main/java/br/com/themoviedb/api/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.themoviedb.api.repository;
-
-import SistemaEscolar.Model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-    User findByEmail(String email);
-}
